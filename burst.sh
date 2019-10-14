fc7a7ecaed7d0f0d94a1dc730b759acaca559ebc
Burr Sutter <burrsutter@gmail.com>
2019-10-14 14:34:02 -0400

    adding some more scripts

---
+++ burst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+INGRESSGATEWAY=istio-ingressgateway
+IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+HOST_URL=$(kubectl get routes.serving.knative.dev burr-greeter  -o jsonpath='{.status.url}')
+STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
+
+siege -r 1 -c 20 -d 2 -v -H "Host: ${STRIPPED}" ${IP_ADDRESS}
